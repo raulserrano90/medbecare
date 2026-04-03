7f88df3d5f5e16edc733b34c96ee22c2b85d278c
raulserrano90 <raulserrano@projectsfull.com>
2026-04-03 21:41:34 +0200

    feat: add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configuración de imágenes para permitir dominios externos en el futuro
+  images: {
+    domains: [],
+  },
+}
+
+module.exports = nextConfig
